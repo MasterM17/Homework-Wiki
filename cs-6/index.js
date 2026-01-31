cf5ee41ff4b16ca13d4fa919f33d75cb8894d36d
MasterM17 <marko.mihajlovic17@gmail.com>
2026-01-30 19:57:18 -0500

    refactor: adjusted handler error logic

--- cs-6/index.js
+++ cs-6/index.js
@@ -13,23 +13,33 @@ const http = require("http");
 
 const handler = (req, res) => {
   const url = req.url;
-  if (url.startsWith("/ime/")) {
-    const [_, op, a] = url.split("/");
-    if (a) {
-      const name = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
-      const karakteri = name.length;
-      const eParno = karakteri % 2 === 0 ? "da" : "ne";
-      const samoglaskiNiza = ["a", "e", "i", "o", "u"];
-      const samoglaskiCount = name
-        .toLowerCase()
-        .split("")
-        .filter((char) => samoglaskiNiza.includes(char)).length;
-      const soglaskiCount = karakteri - samoglaskiCount;
-      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // definirame pravila za browserot
-      // res.end(
-      //   `Zdravo, ova e profilot na ${name}. Parno: ${eParno}, Karakteri: ${karakteri}, Soglaski: ${soglaskiCount}, Samoglaski: ${samoglaskiCount}`,
-      // );
-      res.end(`
+  if (!url.startsWith("/ime/")) {
+    res.end(
+      "Ruta ne e pronajdena, ve molam koristete /ime pred  vnesuvanje na profilot",
+    );
+    return;
+  }
+
+  const [_, op, a] = url.split("/");
+  
+  if (!a) {
+    res.end("Ve molam vnesete ime na profilot po kosata crta");
+    return;
+  }
+  const name = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
+  const karakteri = name.length;
+  const eParno = karakteri % 2 === 0 ? "da" : "ne";
+  const samoglaskiNiza = ["a", "e", "i", "o", "u"];
+  const samoglaskiCount = name
+    .toLowerCase()
+    .split("")
+    .filter((char) => samoglaskiNiza.includes(char)).length;
+  const soglaskiCount = karakteri - samoglaskiCount;
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // definirame pravila za browserot
+  // res.end(
+  //   `Zdravo, ova e profilot na ${name}. Parno: ${eParno}, Karakteri: ${karakteri}, Soglaski: ${soglaskiCount}, Samoglaski: ${samoglaskiCount}`,
+  // );
+  res.end(`
   <div style="font-family: sans-serif; padding: 20px; border: 2px solid #333; border-radius: 10px; max-width: 400px;">
     <h1 style="color: #007bff;">Profil na ${name}</h1>
     <ul style="list-style: none; padding: 0; line-height: 2;">
@@ -40,14 +50,6 @@ const handler = (req, res) => {
     </ul>
   </div>
 `);
-    } else {
-      res.end("Ve molam vnesete ime na profilot po kosata crta");
-    }
-  } else {
-    res.end(
-      "Ruta ne e pronajdena, ve molam koristete /ime pred  vnesuvanje na profilot",
-    );
-  }
 };
 
 const server = http.createServer(handler);
