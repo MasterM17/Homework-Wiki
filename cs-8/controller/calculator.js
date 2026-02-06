14ab0c5dc51d05f0905ecf2ac3ef2bfe4152d639
MasterM17 <marko.mihajlovic17@gmail.com>
2026-02-05 20:06:30 -0500

    ref: double calculator added

--- cs-8/controller/calculator.js
+++ cs-8/controller/calculator.js
@@ -1,36 +1,69 @@
 const utils = require("../utils/index");
 
+const calculators = [
+  { value: "force", name: "Force (F = m * a)" },
+  { value: "momentum", name: "Momentum (p = m * v)" },
+];
+
 const getMassCalculator = async (req, res) => {
   try {
+    const calculatorOptions = calculators
+      .map((options) => {
+        return `<option value="${options.value}">${options.name}</option>`;
+      })
+      .join("");
     const response = await utils.parseTemplate("calculator.html", {
       result: "",
+
+      title: "",
+      label1: "Mass",
+      label2: "Number",
     });
     res.send(response);
   } catch (error) {
-    console.log("Critical error", err);
+    console.log("Critical error", error);
     res.status(500).send("Something went wrong ! Please try again !");
   }
 };
 
 const postMassCalculator = async (req, res) => {
   try {
+    const { input1, input2, calcType } = req.body;
     // error
-    if (req.body.mass === "" || req.body.acceleration === "") {
+    if (!input1 || !input2) {
       return res.status(400).send("Bad request!");
     }
     //logika
-    const mass = Number(req.body.mass);
-    const accel = Number(req.body.acceleration);
-    if (mass <= 0) {
+    console.log(calcType);
+    if (input1 <= 0) {
       const response = await utils.parseTemplate("calculator.html", {
         result: "Error: Masata mora da bide pogolema 0 kg.",
       });
       return res.status(400).send(response);
     }
-    const force = mass * accel;
+    let rezultat = 0;
+    switch (calcType) {
+      case "force":
+        rezultat = Number(input1) * Number(input2);
+        break;
 
+      case "momentum":
+        rezultat = Number(input1) * Number(input2);
+        break;
+    }
+    const calculatorOptions = calculators
+      .map((opt) => {
+        const isSelected = opt.value === calcType ? "selected" : "";
+        return `<option value="${opt.value}" ${isSelected}>${opt.name}</option>`;
+      })
+      .join("");
+    console.log("Calculated:", rezultat);
     const response = await utils.parseTemplate("calculator.html", {
-      result: force,
+      result: rezultat,
+
+      calcOptions: calculatorOptions,
+      label1: "Mass (kg)",
+      label2: calcType === "force" ? "Acceleration (m/s)" : "Velocity (m/s)",
     });
 
     res.send(response);
