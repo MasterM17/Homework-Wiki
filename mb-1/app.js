f1b6ae1f0a7845ced99ca1edcecb5fe8001b082b
MasterM17 <marko.mihajlovic17@gmail.com>
2026-02-13 06:18:52 +0100

    Update app.js

--- mb-1/app.js
+++ mb-1/app.js
@@ -8,16 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose
-  .connect(
-    "mongodb+srv://MasterM17_db_user:Rx4xksriqHCSPe@cluster0.hwfkhyb.mongodb.net/semos1?appName=Cluster0",
-  )
-  .then(() => {
-    console.log("Sucesfully connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+
 app.get("/api/v1/landing", getLanding);
 app.get("/api/v1/products", allProducts);
 app.post("/api/v1/products", addProduct);
